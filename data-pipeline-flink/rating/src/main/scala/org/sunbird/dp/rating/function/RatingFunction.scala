cb5cf1f272f5a44c995bc7901cd77a2c54bf9e50
vikrantsingh <vikrant.bipin@tarento.com>
2025-09-26 12:13:33 +0530

    Fixed exception trace logging

--- data-pipeline-flink/rating/src/main/scala/org/sunbird/dp/rating/function/RatingFunction.scala
+++ data-pipeline-flink/rating/src/main/scala/org/sunbird/dp/rating/function/RatingFunction.scala
@@ -125,7 +125,7 @@ class RatingFunction(config: RatingConfig, @transient var cassandraUtil: Cassand
       case ex: Exception =>
         context.output(config.failedEvent, event)
         // log full message + stack trace through logger
-        logger.error(s"Exception while processing eventId=${event.activityId}", ex)
+        logger.error(s"Exception while processing event for activityId=${event.activityId}", ex)
     }
   }
 
